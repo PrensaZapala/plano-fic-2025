a0118701240a0b080b1d442259b79af5b1cb27e7
Maezno <maezno@gmail.com>
2025-09-26 15:07:25 -0300

    stands y foodtrucks

--- script.js
+++ script.js
@@ -63,50 +63,60 @@ let initialPanY = 0;
 // DATOS DE LOS STANDS
 // =======================================================
 const interactiveAreasData = [
-  { id: "stand-1", "x": 544.0, "y": 281, "name": "Argentina", "flag": "https://flagcdn.com/w80/ar.png", "info": "Gastronomía argentina, vinos y productos tradicionales" },
-  { id: "stand-2", "x": 603.0, "y": 281, "name": "Brasil", "flag": "https://flagcdn.com/w80/br.png", "info": "Café brasileño, caipirinha y productos tropicales" },
-  { id: "stand-3", "x": 662.0, "y": 281, "name": "Chile", "flag": "https://flagcdn.com/w80/cl.png", "info": "Vinos chilenos, empanadas y productos del mar" },
-  { id: "stand-4", "x": 721.0, "y": 281, "name": "México", "flag": "https://flagcdn.com/w80/mx.png", "info": "Tacos, tequila y artesanías mexicanas" },
-  { id: "stand-5", "x": 780.0, "y": 281, "name": "Perú", "flag": "https://flagcdn.com/w80/pe.png", "info": "Ceviche, pisco y productos andinos" },
-  { id: "stand-6", "x": 839.0, "y": 281, "name": "Colombia", "flag": "https://flagcdn.com/w80/co.png", "info": "Café colombiano, arepas y productos artesanales" },
-  { id: "stand-7", "x": 898.0, "y": 281, "name": "España", "flag": "https://flagcdn.com/w80/es.png", "info": "Jamón ibérico, aceite de oliva y productos españoles" },
-  { id: "stand-8", "x": 957.0, "y": 281, "name": "Italia", "flag": "https://flagcdn.com/w80/it.png", "info": "Pasta, pizza y productos italianos auténticos" },
-  { id: "stand-9", "x": 1016.0, "y": 281, "name": "Francia", "flag": "https://flagcdn.com/w80/fr.png", "info": "Quesos franceses, vinos y productos gourmet" },
-  { id: "stand-10", "x": 1075.0, "y": 281, "name": "Alemania", "flag": "https://flagcdn.com/w80/de.png", "info": "Cerveza alemana, salchichas y productos tradicionales" },
-  { id: "stand-11", "x": 1134.0, "y": 281, "name": "Reino Unido", "flag": "https://flagcdn.com/w80/gb.png", "info": "Té inglés, fish & chips y productos británicos" },
-  { id: "stand-12", "x": 1193.0, "y": 281, "name": "Japón", "flag": "https://flagcdn.com/w80/jp.png", "info": "Sushi, sake y productos japoneses tradicionales" },
-  { id: "stand-13", "x": 1252.0, "y": 281, "name": "China", "flag": "https://flagcdn.com/w80/cn.png", "info": "Dim sum, té chino y productos orientales" },
-  { id: "stand-14", "x": 1311, "y": 340, "name": "India", "flag": "https://flagcdn.com/w80/in.png", "info": "Especias indias, curry y productos ayurvédicos" },
-  { id: "stand-15", "x": 1311, "y": 399, "name": "Tailandia", "flag": "https://flagcdn.com/w80/th.png", "info": "Pad thai, curry tailandés y productos asiáticos" },
-  { id: "stand-16", "x": 1311, "y": 458, "name": "Turquía", "flag": "https://flagcdn.com/w80/tr.png", "info": "Baklava, café turco y productos mediterráneos" },
-  { id: "stand-17", "x": 1311, "y": 517, "name": "Grecia", "flag": "https://flagcdn.com/w80/gr.png", "info": "Aceite de oliva, feta y productos mediterráneos" },
-  { id: "stand-18", "x": 1311, "y": 576, "name": "Marruecos", "flag": "https://flagcdn.com/w80/ma.png", "info": "Tagine, té de menta y artesanías marroquíes" },
-  { id: "stand-19", "x": 1311, "y": 846, "name": "Estados Unidos", "flag": "https://flagcdn.com/w80/us.png", "info": "BBQ, hamburguesas y productos americanos" },
-  { id: "stand-20", "x": 1311, "y": 905, "name": "Canadá", "flag": "https://flagcdn.com/w80/ca.png", "info": "Jarabe de maple, poutine y productos canadienses" },
-  { id: "stand-21", "x": 1311, "y": 964, "name": "Australia", "flag": "https://flagcdn.com/w80/au.png", "info": "Vinos australianos, carne y productos del Pacífico" },
-  { id: "stand-22", "x": 1311, "y": 1023, "name": "Sudáfrica", "flag": "https://flagcdn.com/w80/za.png", "info": "Biltong, rooibos y productos sudafricanos" },
-  { id: "stand-23", "x": 1311, "y": 1082, "name": "Egipto", "flag": "https://flagcdn.com/w80/eg.png", "info": "Dátiles, té de hibisco y productos árabes" },
-  { id: "stand-24", "x": 1311, "y": 1141, "name": "Rusia", "flag": "https://flagcdn.com/w80/ru.png", "info": "Caviar, vodka y productos rusos tradicionales" },
-  { id: "stand-25", "x": 1311, "y": 1200, "name": "Corea del Sur", "flag": "https://flagcdn.com/w80/kr.png", "info": "Kimchi, bulgogi y productos coreanos" },
-  { id: "stand-26", "x": 1311, "y": 1259, "name": "Vietnam", "flag": "https://flagcdn.com/w80/vn.png", "info": "Pho, café vietnamita y productos asiáticos" },
-  { id: "stand-27", "x": 1252, "y": 1318, "name": "Indonesia", "flag": "https://flagcdn.com/w80/id.png", "info": "Rendang, café de Java y especias asiáticas" },
-  { id: "stand-28", "x": 1193, "y": 1318, "name": "Filipinas", "flag": "https://flagcdn.com/w80/ph.png", "info": "Adobo, lumpia y productos filipinos" },
-  { id: "stand-29", "x": 1134, "y": 1318, "name": "Malasia", "flag": "https://flagcdn.com/w80/my.png", "info": "Nasi lemak, satay y productos malayos" },
-  { id: "stand-30", "x": 1075, "y": 1318, "name": "Singapur", "flag": "https://flagcdn.com/w80/sg.png", "info": "Chili crab, laksa y fusión asiática" },
-  { id: "stand-31", "x": 780, "y": 1318, "name": "Uruguay", "flag": "https://flagcdn.com/w80/uy.png", "info": "Asado uruguayo, dulce de leche y productos ganaderos" },
-  { id: "stand-32", "x": 721, "y": 1318, "name": "Paraguay", "flag": "https://flagcdn.com/w80/py.png", "info": "Sopa paraguaya, tereré y productos tradicionales" },
-  { id: "stand-33", "x": 662, "y": 1318, "name": "Bolivia", "flag": "https://flagcdn.com/w80/bo.png", "info": "Quinoa, llajua y productos andinos" },
-  { id: "stand-34", "x": 603, "y": 1318, "name": "Ecuador", "flag": "https://flagcdn.com/w80/ec.png", "info": "Cacao ecuatoriano, ceviche y productos amazónicos" },
-  { id: "stand-35", "x": 544, "y": 1318, "name": "Venezuela", "flag": "https://flagcdn.com/w80/ve.png", "info": "Arepas, pabellón y productos venezolanos" },
-  // Nuevos stands del centro
-  { id: "stand-36", "x": 1081, "y": 767, "name": "Stand Central 1", "flag": "https://flagcdn.com/w80/xx.png", "info": "Información de Stand Central 1" },
-  { id: "stand-37", "x": 1081, "y": 826, "name": "Stand Central 2", "flag": "https://flagcdn.com/w80/xx.png", "info": "Información de Stand Central 2" },
-  { id: "stand-38", "x": 999, "y": 767, "name": "Stand Central 3", "flag": "https://flagcdn.com/w80/xx.png", "info": "Información de Stand Central 3" },
-  { id: "stand-39", "x": 999, "y": 826, "name": "Stand Central 4", "flag": "https://flagcdn.com/w80/xx.png", "info": "Información de Stand Central 4" },
-  { id: "stand-40", "x": 917, "y": 767, "name": "Stand Central 5", "flag": "https://flagcdn.com/w80/xx.png", "info": "Información de Stand Central 5" },
-  { id: "stand-41", "x": 917, "y": 826, "name": "Stand Central 6", "flag": "https://flagcdn.com/w80/xx.png", "info": "Información de Stand Central 6" },
-  { id: "stand-42", "x": 835, "y": 767, "name": "Stand Central 7", "flag": "https://flagcdn.com/w80/xx.png", "info": "Información de Stand Central 7" },
-  { id: "stand-43", "x": 835, "y": 826, "name": "Stand Central 8", "flag": "https://flagcdn.com/w80/xx.png", "info": "Información de Stand Central 8" }
+  { id: "stand-1", x: 1310.6, y: 1199.6, width: 58, height: 117, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-2", x: 543.6, y: 280.6, width: 117, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-3", x: 661.6, y: 280.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-4", x: 720.6, y: 280.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-5", x: 779.6, y: 280.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-6", x: 838.6, y: 280.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-7", x: 897.6, y: 280.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-8", x: 850.1, y: 768.9, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-9", x: 909.1, y: 768.9, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-10", x: 968.1, y: 768.9, width: 58, height: 117, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-11", x: 956.6, y: 280.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-12", x: 1015.6, y: 280.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-13", x: 1074.6, y: 280.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-14", x: 1133.6, y: 280.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-15", x: 1192.6, y: 280.6, width: 117, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-16", x: 1310.6, y: 339.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-17", x: 1310.6, y: 398.6, width: 58, height: 117, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-18", x: 1310.6, y: 516.6, width: 58, height: 117, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-19", x: 1310.6, y: 845.6, width: 58, height: 235, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-20", x: 1310.6, y: 1081.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-21", x: 1310.6, y: 1140.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-22", x: 1192.6, y: 1317.6, width: 117, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-23", x: 1133.6, y: 1317.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-24", x: 1074.6, y: 1317.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-25", x: 661.6, y: 1317.6, width: 117, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-26", x: 602.6, y: 1317.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-27", x: 356.2, y: 1494.1, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-28", x: 356.2, y: 1697.4, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-29", x: 356.2, y: 1957.1, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-30", x: 535.1, y: 2236.7, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-31", x: 1002.8, y: 2236.7, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-32", x: 1412.5, y: 2038.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-33", x: 1412.5, y: 1728, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-34", x: 1412.5, y: 1528.6, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-35", x: 1412.5, y: 1655.2, width: 58, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-36", x: 1196.6, y: 2236.7, width: 78, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-37", x: 728.8, y: 2236.7, width: 78.5, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-38", x: 814.9, y: 2236.7, width: 116.3, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-39", x: 1072.2, y: 2236.7, width: 116.3, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-40", x: 940.2, y: 2236.7, width: 55.3, height: 38.4, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-41", x: 374.7, y: 1894.8, width: 39.5, height: 55.1, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-42", x: 374.7, y: 2150.2, width: 39.5, height: 60, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-43", x: 374.4, y: 1575.8, width: 39.9, height: 117.8, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-44", x: 374.4, y: 1765.5, width: 39.9, height: 117.8, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-45", x: 374.4, y: 2022.4, width: 39.9, height: 117.8, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-46", x: 1412.5, y: 2102.4, width: 39.9, height: 137.3, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-47", x: 1412.5, y: 1919.7, width: 39.9, height: 109.9, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-48", x: 1412.5, y: 1790.2, width: 39.9, height: 117.9, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-49", x: 1412.5, y: 1593.2, width: 39.9, height: 55.2, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-50", x: 413.9, y: 2236.7, width: 117.8, height: 39.9, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-51", x: 603.4, y: 2236.7, width: 117.8, height: 39.9, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-52", x: 1278.6, y: 2236.7, width: 117.8, height: 39.9, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-53", x: 485.9, y: 1317.6, width: 115.7, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" },
+  { id: "stand-54", x: 850.1, y: 827.9, width: 117, height: 58, name: "", flag: "https://flagcdn.com/w80/zz.png", info: "" }
 ];
 
 // =======================================================
@@ -119,8 +129,8 @@ function applyTransform() {
 
 function calculateMinZoom() {
   const containerRect = interactiveContainer.getBoundingClientRect();
-  const mapWidth = 3916.9;
-  const mapHeight = 2326.6;
+  const mapWidth = 3916;
+  const mapHeight = 2586;
   const scaleX = containerRect.width / mapWidth;
   const scaleY = containerRect.height / mapHeight;
   return Math.min(scaleX, scaleY) * 0.9;
@@ -133,8 +143,8 @@ function setInitialView() {
     // Coordenadas de la zona que quieres mostrar (del plano SVG)
     const viewboxX = 215.9;
     const viewboxY = 82.9;
-    const viewboxWidth = 1401.3;
-    const viewboxHeight = 2240.3;
+    const viewboxWidth = 1400;
+    const viewboxHeight = 2300;
 
     // Calcular el nivel de zoom para que el área encaje en el contenedor
     const scaleX = containerRect.width / viewboxWidth;
@@ -206,6 +216,10 @@ function createInteractiveArea(area) {
   areaElement.classList.add('interactive-area');
   areaElement.style.left = `${area.x}px`;
   areaElement.style.top = `${area.y}px`;
+  areaElement.style.left = `${area.x}px`;
+areaElement.style.top = `${area.y}px`;
+areaElement.style.width = `${area.width}px`;
+areaElement.style.height = `${area.height}px`;
 
   const flagImg = document.createElement('img');
   flagImg.src = area.flag;
